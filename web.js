1582d3ae80d4b3d7871364d76ad87a0336fa3f71
eleomir <l.aziukiewicz@gmail.com>
2013-07-29 12:28:59 +0000

    poprawki web.js 3

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   
-  var buffer = new Buffer;
+  var buffer = new Buffer(30);
 
   buffer.write(fs.readFileSync('index.html'));
   response.send(buffer.tostring());
